bb08d6762a55b64f76963bf41a96693ff6ab9d9f
RyoMaeda2525 <maetyuryu@gmail.com>
2022-11-11 15:21:32 +0900

    [add]Textファイルから文章とコマンドを分け文章を表示するようにした

--- Assets/NovelGame/Scripts/NovelInput.cs
+++ Assets/NovelGame/Scripts/NovelInput.cs
@@ -1,24 +1,99 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+using System.Text.RegularExpressions;
+using UnityEngine.UI;
 
 public class NovelInput : MonoBehaviour
 {
     [SerializeField, Tooltip("テキストウィンドウにメッセージを表示するScript")]
-    MessageText _mt = default;
+    MessagePrinter _printer = default;
 
     [SerializeField, Tooltip("背景を操作するScript")]
     BackGround _bg = default;
 
+    [SerializeField, Tooltip("メッセージ情報を持つTextファイル")]
+    private string _loadFileName;
+
+    private string[] _scenarios;
+
+    /// <summary>現在読み込んでいる行</summary>
+    private int _currentLine = 0;
+
+    private Regex regex = new Regex("@(\\S+)\\s");
+
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        UpdateLines(_loadFileName);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        MoveNext();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (_printer.IsPrinting) { _printer.Skip(); }
+            else { MoveNext(); }
+        }
+    }
+
+    public void UpdateLines(string fileName)
+    {
+        var scenarioText = Resources.Load<TextAsset>("Scenario/" + fileName);
+
+        if (scenarioText == null)
+        {
+            Debug.LogError("シナリオファイルが見つかりませんでした");
+            Debug.LogError("ScenarioManagerを無効化します");
+            enabled = false;
+            return;
+        }
+        _scenarios = scenarioText.text.Split(new string[] { "@br" }, StringSplitOptions.None);
+        _currentLine = 0;
+
+        Resources.UnloadAsset(scenarioText);
+    }
+
+    /// <summary>
+    /// 次のページに進む。
+    /// 次のページが存在しない場合は無視する。
+    /// </summary>
+    private void MoveNext()
+    {
+        if (_scenarios is null or { Length: 0 }) { return; }
+
+        if (_currentLine + 1 < _scenarios.Length)
+        {
+            _currentLine++;
+
+            int indexof = _scenarios[_currentLine].IndexOf("//");
+
+            if (indexof != -1) 
+            {
+                _scenarios[_currentLine] = _scenarios[_currentLine].Substring(0, indexof);
+            }
+
+            String[] commandText;
+
+            commandText = _scenarios[_currentLine].Split(new string[] { "@" }, StringSplitOptions.None);
+
+            Command(commandText);
+
+            _printer?.ShowMessage(commandText[commandText.Length - 1]);
+        }
+    }
+
+    private void Command(string[] commandText) 
+    {
+        for(int i = 0; i < commandText.Length - 1; i++) 
+        {
+            Debug.Log(commandText[i]);
+        }
     }
 }
